ab5b99dfbb2a8cf79901e464fd4b28c529737ed5
Nathan Pham <nathanpham.me@gmail.com>
2021-05-31 13:54:20 -0700

    resize video

--- chapter-2/estimatepose.py
+++ chapter-2/estimatepose.py
@@ -2,3 +2,27 @@ import mediapipe as mp
 import cv2
 
 camera = cv2.VideoCapture("videos/girl-scooter.mp4")
+window_name = "pose estimation"
+
+cv2.namedWindow(window_name)
+cv2.startWindowThread()
+
+def resize_aspect(img, width=None, height=None):
+    (o_height, o_width) = img.shape[:2]
+    dimensions = [0, 0]
+    
+    if width:
+        ratio = width / float(o_width)
+        dimensions = (width, (int(o_height * ratio)))
+    elif height:
+        ratio = height / float(o_height)
+        dimensions = (int(o_width * ratio), height)
+
+    return cv2.resize(img, dimensions, interpolation=cv2.INTER_AREA)
+
+while cv2.getWindowProperty(window_name, 0) >= 0:
+    success, img = camera.read()
+    resized_img = resize_aspect(img=img, height=600)
+
+    cv2.imshow(window_name, resized_img)
+    key_code = cv2.waitKey(50)
